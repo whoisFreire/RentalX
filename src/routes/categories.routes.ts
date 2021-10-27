1bd328252dd2146c8d06b7fd45d9740f5c1d10a9
Leonardo Freire <leonardofreire.russi@icloud.com>
2021-10-27 13:42:27 -0300

    add upload categories list file feature

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-
+const upload = multer({
+  dest: './tmp',
+});
 categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
 
 categoriesRoutes.get('/', (request, response) => listCategoryController.handle(request, response));
 
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => importCategoryController.handle(request, response));
 export { categoriesRoutes };
