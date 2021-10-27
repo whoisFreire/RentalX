98bc87da7e3485594d0a1b3dd49c130361b342bd
Leonardo Freire <leonardofreire.russi@icloud.com>
2021-10-27 11:27:46 -0300

    first commit: Rentalx

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoryController } from '../modules/cars/useCases/listCategories';
+
+const categoriesRoutes = Router();
+
+categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request, response));
+
+categoriesRoutes.get('/', (request, response) => listCategoryController.handle(request, response));
+
+export { categoriesRoutes };
